9b05e31a0ed73274919384b32fc4dcbcac835c08
Jacob Goodman <jswag7890@gmail.com>
2025-04-19 19:13:18 -0700

    finished all main parts of page, needs manifest and more optional detail

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('toggleMode');
+const toggleButton = document.getElementById('toggleMode');
+// Toggle light and dark modes
+const updateText = () => {
+    if (document.body.classList.contains('dark-mode')) {
+    toggleButton.textContent = 'Switch to Light Mode';
+    } else {
+    toggleButton.textContent = 'Switch to Dark Mode';
+    }
+};
 
-    const updateText = () => {
-      if (document.body.classList.contains('dark-mode')) {
-        toggleButton.textContent = 'Switch to Light Mode';
-      } else {
-        toggleButton.textContent = 'Switch to Dark Mode';
-      }
-    };
-  
-    toggleButton.addEventListener('click', () => {
-      document.body.classList.toggle('dark-mode');
-      updateText();
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    updateText();
+});
+
+updateText(); 
+});
+
+const buttons = document.querySelectorAll('.expand-btn');
+// Expand PinguType card when viewing project
+buttons.forEach(button => {
+button.addEventListener('click', () => {
+    const card = button.closest('.project-card');
+    const grid = card.closest('.projects-grid');
+    
+    const isExpanding = !card.classList.contains('expanded');
+
+    document.querySelectorAll('.project-card.expanded').forEach(expandedCard => {
+    expandedCard.classList.remove('expanded');
     });
-  
-    updateText(); 
-  });
-  
+
+    if (!isExpanding) {
+    grid.classList.remove('expanded-mode');
+    button.textContent = 'Show Project';
+    return;
+    }
+
+    card.classList.add('expanded');
+    grid.classList.add('expanded-mode');
+    button.textContent = 'Hide Project';
+});
+});
+
+
+let scrollLockTop = null;
+
+
+const iframe = document.querySelector('#pingutype-iframe');
+
+// Prevent auto-scrolling when using iFrame
+iframe.addEventListener('mouseenter', () => {
+scrollLockTop = window.scrollY;
+});
+
+iframe.addEventListener('mouseleave', () => {
+scrollLockTop = null;
+});
+
+window.addEventListener('scroll', () => {
+if (scrollLockTop !== null) {
+    window.scrollTo({
+    top: scrollLockTop,
+    behavior: 'instant'
+    });
+}
+});
